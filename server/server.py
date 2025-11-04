5289c81392eea88d6a0f3f585877c6dacaa45717
1Axen <github@axen.dev>
2025-11-04 16:54:52 +0200

    use asyncio for threading

--- server/server.py
+++ server/server.py
@@ -1,13 +1,19 @@
 import socketio
+import asyncio
 from aiohttp import web
 
 from router import route
 
-sio = socketio.AsyncServer()
-app = web.Application()
-
-if __name__ == "__main__":
+async def main():
+    app = web.Application()
     sio.attach(app)
     app.router.add_get(r"/{name:.*}", route)
-    web.run_app(app)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
