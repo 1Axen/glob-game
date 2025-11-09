5cc5a939b9d531b7fbcea386d68874ca9fbdb44d
1Axen <github@axen.dev>
2025-11-09 07:32:49 +0200

    refactor python game module into a class

--- server/game.py
+++ server/game.py
@@ -1,14 +1,13 @@
 import socketio
 from ecs import World, Id, Query, tag, component
 import vector
+from config import Config
 from time import time
 from asyncio import sleep
 from typing import Dict
 
 Vector = vector.Vector
-
-sio = socketio.AsyncServer()
-world = World()
+SocketServer = socketio.AsyncServer
 
 # types
 Food = tag()
@@ -23,55 +22,83 @@ Position = component()
 Velocity = component()
 MoveDirection = component()
 
-SID_ENTITY_MAP: Dict[str, Id] = {}
+def create_glob(world: World, mass: int, position: Vector) -> Id:
+    glob = world.entity()
+    world.set(glob, Mass, mass)
+    world.set(glob, Position, position)
+    return glob
 
-@sio.event
-def connect(sid, environ):
-    print(f"connect: {sid}")
-
-@sio.event
-def disconnect(sid):
-    print("disconnect:", sid)
-    entity = SID_ENTITY_MAP.get(sid)
-    if entity == None:
-        return
-    
-    world.delete(entity)
-    print(f"-> deleted entity: {entity}")    
-
-@sio.event
-def respawn(sid, name: str):
-    existing = SID_ENTITY_MAP.pop(sid, None)
-    if existing != None:
-        world.delete(existing)
-
-    entity = world.entity()
-    world.set(entity, Name, name)
-    world.set(entity, Mass, 10)
-    world.set(entity, Position, Vector(0, 0))
-    world.set(entity, MoveDirection, Vector(0, 1))
-    world.set(entity, Session, sid)
-    SID_ENTITY_MAP[sid] = entity
-
-    print(f"-> created entity: {entity} ({name})")
-
-def move_players(delta_time: float):
-    for entity, mass, position, move_direction in Query(world, [Mass, Position, MoveDirection]):
+def move_globs(world: World, delta_time: float):
+    for entity, mass, position, move_direction in Query(world, Mass, Position, MoveDirection):
         speed = mass
         velocity = Vector(move_direction.x * speed * delta_time, move_direction.y * speed * delta_time)
         position = Vector(position.x + velocity.x, position.y + velocity.y)
         world.set(entity, Position, position)
 
-async def game_loop():
-    last_time = time()
-    tick_rate = (1.0 / 20.0)
+class GameInstance():
+    world: World
+    config: Config
+    socket: SocketServer
+    _entity_map: Dict[str, Id]
 
-    while True:
-        curr_time = time()
-        delta_time = curr_time - last_time
-        last_time = curr_time
+    def __init__(self, socket: SocketServer, world: World, config: Config) -> None:
+        self.world = world
+        self.config = config
+        self.socket = socket
 
-        move_players(delta_time)
+        pass
 
-        elapsed = time() - curr_time
-        await sleep(max(0, tick_rate - elapsed))
+    def connect(self, sid: str, environ):
+        world = self.world
+        entity = world.entity()
+        world.set(entity, Session, sid)
+        self._entity_map[sid] = entity
+
+    def disconnect(self, sid: str):
+        world = self.world
+        entity_map = self._entity_map
+
+        entity = entity_map.get(sid)
+        if entity == None:
+            return
+        
+        entity_map.pop(sid)
+        
+        for child, _ in Query(world, entity):
+            world.delete(child)
+                
+        world.delete(entity)
+
+        print(f"* deleted entity: {entity}")    
+
+    def respawn(self, sid: str, name: str):
+        entity = self._entity_map.get(sid, None)
+        if entity == None:
+            raise Exception
+        
+        world = self.world
+        world.set(entity, Name, name)
+
+        glob = create_glob(world, 10, Vector())
+        world.add(glob, entity)
+        world.set(glob, Velocity, Vector())
+        world.set(glob, MoveDirection, Vector())
+
+        print(f"* created entity: {entity} ({name})")
+
+    async def init_game_loop(self):
+        world = self.world
+        socket = self.socket
+
+        last_time = time()
+        tick_rate = (1.0 / 20.0)
+
+        while True:
+            curr_time = time()
+            delta_time = curr_time - last_time
+            last_time = curr_time
+
+            move_globs(world, delta_time)
+
+            elapsed = time() - curr_time
+            await sleep(max(0, tick_rate - elapsed))
--- server/server.py
+++ server/server.py
@@ -2,14 +2,15 @@ import socketio
 import asyncio
 import os
 from config import Config
+from ecs import World
 from aiohttp import web
-
-from game import sio, game_loop
 from router import route
+from game import GameInstance
 
 config = Config.from_file(os.path.join(os.path.dirname(__file__), "../config.json"))
 
 async def main():
+    sio = socketio.AsyncServer()
     app = web.Application()
     sio.attach(app)
     app.router.add_get(r"/{name:.*}", route)
@@ -19,7 +20,24 @@ async def main():
     site = web.TCPSite(runner, "0.0.0.0", 8080)
     await site.start()
 
-    await game_loop()
+    world = World()
+    game_instance = GameInstance(sio, world, config)
+
+    @sio.event
+    def connect(sid, environ):
+        print(f"connect {sid}")
+        game_instance.connect(sid, environ)
+
+    @sio.event
+    def disconnect(sid):
+        print(f"disconnect {sid}")
+        game_instance.disconnect(sid)
+
+    @sio.event
+    def respawn(sid, name):
+        game_instance.respawn(sid, name)
+
+    await game_instance.init_game_loop()
 
 if __name__ == "__main__":
     asyncio.run(main())
