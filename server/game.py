b3923a71682cb73e88bf493736220354883ff412
1Axen <github@axen.dev>
2025-11-19 13:28:44 +0200

    store game config in world instead of entire config

--- server/game.py
+++ server/game.py
@@ -4,7 +4,7 @@ import random
 import json
 import libs.vector as vector
 from libs.vector_map import VectorMap
-from libs.config import Config
+from libs.config import Config, GameConfig
 from time import time
 from asyncio import sleep
 from typing import Dict
@@ -13,8 +13,8 @@ Vector = vector.Vector
 SocketServer = socketio.AsyncServer
 
 # singletons
-GameConfig = component(Config)
-FoodVectorMap = component(VectorMap)
+VectorMapSingleton = component(VectorMap)
+GameConfigSingleton = component(GameConfig)
 
 # types
 Food = tag()
@@ -44,23 +44,22 @@ def clamp(x: float, min: float, max: float) -> float:
         return min
     return x
 
-def point_to_vector(config: Config, point: tuple[float, float]) -> Vector:
-    max_x = config.game.width / 2
-    max_y = config.game.height / 2
+def point_to_vector(config: GameConfig, point: tuple[float, float]) -> Vector:
+    max_x = config.width / 2
+    max_y = config.height / 2
     return Vector(
         clamp(point[0], -max_x, max_x),
         clamp(point[1], -max_y, max_y)
     )
 
-def mass_to_radius(config: Config, mass: float) -> float:
-    return config.game.base_radius + mass * config.game.mass_radius_consant
+def mass_to_radius(config: GameConfig, mass: float) -> float:
+    return config.base_radius + mass * config.mass_radius_consant
 
-def speed_from_mass(config: Config, mass: float) -> float:
-    game_config = config.game
+def speed_from_mass(config: GameConfig, mass: float) -> float:
     return map(
         mass, 
-        game_config.minimum_mass, game_config.maximum_mass, 
-        game_config.maximum_speed, game_config.minimum_speed
+        config.minimum_mass, config.maximum_mass, 
+        config.maximum_speed, config.minimum_speed
     )
 
 def can_eat_glob(position: Vector, radius: float, other_position: Vector, other_radius: float) -> bool:
@@ -74,15 +73,15 @@ def create_glob(world: World, mass: float, position: Vector) -> Id:
     world.set(glob, Position, position)
     return glob
 
-def spawn_food(world: World, config: Config, vector_map: VectorMap) -> Id:
-    half_width = config.game.width // 2
-    half_height = config.game.height // 2
+def spawn_food(world: World, config: GameConfig, vector_map: VectorMap) -> Id:
+    half_width = config.width // 2
+    half_height = config.height // 2
     position = Vector(
         random.randint(-half_width, half_width), 
         random.randint(-half_height, half_height)
     )
 
-    entity = create_glob(world, config.game.food_mass, position)
+    entity = create_glob(world, config.food_mass, position)
     world.add(entity, Food)
     vector_map.insert(entity, position)
 
@@ -98,8 +97,8 @@ def spawn_player(world: World, parent: Id, mass: float, position: Vector) -> Id:
     return glob
 
 def eat_food(world: World):
-    config = world.get(GameConfig, GameConfig)
-    vector_map = world.get(FoodVectorMap, FoodVectorMap)
+    config = world.get(GameConfigSingleton, GameConfigSingleton)
+    vector_map = world.get(VectorMapSingleton, VectorMapSingleton)
 
     assert config != None
     assert vector_map != None
@@ -131,7 +130,7 @@ def eat_food(world: World):
         world.set(entity, Mass, mass)
 
 def eat_players(world: World, delta_time: float):
-    config = world.get(GameConfig, GameConfig)
+    config = world.get(GameConfigSingleton, GameConfigSingleton)
     assert config != None
 
     for entity, mass, position, parent in Query(world, Mass, Position, Parent):
@@ -157,29 +156,29 @@ def eat_players(world: World, delta_time: float):
         world.set(entity, Mass, mass)
 
 def update_velocity(world: World, delta_time: float):
-    config = world.get(GameConfig, GameConfig)
+    config = world.get(GameConfigSingleton, GameConfigSingleton)
     assert config != None
 
     for entity, mass, velocity in Query(world, Mass, Velocity):
-        velocity = vector.friction(velocity, config.game.friction, delta_time)
+        velocity = vector.friction(velocity, config.friction, delta_time)
 
         direction = world.get(entity, MoveDirection)
         if (direction != None):
             speed = speed_from_mass(config, mass)
             if (vector.magnitude(velocity) <= speed):
-                velocity = vector.accelerate(velocity, direction, speed, config.game.acceleration, delta_time)
+                velocity = vector.accelerate(velocity, direction, speed, config.acceleration, delta_time)
 
         world.set(entity, Velocity, velocity)
 
 def update_positions(world: World, delta_time: float):
-    config = world.get(GameConfig, GameConfig)
-    vector_map = world.get(FoodVectorMap, FoodVectorMap)
+    config = world.get(GameConfigSingleton, GameConfigSingleton)
+    vector_map = world.get(VectorMapSingleton, VectorMapSingleton)
 
     assert config != None
     assert vector_map != None
 
-    half_width = config.game.width / 2
-    half_height = config.game.height / 2
+    half_width = config.width / 2
+    half_height = config.height / 2
 
     for entity, mass, position, velocity in Query(world, Mass, Position, Velocity):
         old_position = position
@@ -236,6 +235,7 @@ def serialize_world(world: World, server_time: float) -> str:
     for entity, mass, position in Query(world, Mass, Position):
         parent = world.get(entity, Parent)
         player_index = -1
+        
         if parent != None:
             player_index = player_index_map.get(parent)
 
@@ -245,25 +245,29 @@ def serialize_world(world: World, server_time: float) -> str:
 
 class GameInstance():
     world: World
-    config: Config
     socket: SocketServer
-    vector_map: VectorMap
+    game_config: GameConfig
+
+    _tick_rate: float 
     _entity_map: Dict[str, Id]
+    _food_vector_map: VectorMap
 
     def __init__(self, socket: SocketServer, world: World, config: Config) -> None:
         vector_map = VectorMap()
+        game_config = config.game
 
         self.world = world
-        self.config = config
         self.socket = socket
-        self.vector_map = vector_map
+        self.game_config = game_config
+        
+        self.tick_rate = (1 / config.server.update_rate)
         self._entity_map = {}
         
-        world.set(GameConfig, GameConfig, config)
-        world.set(FoodVectorMap, FoodVectorMap, vector_map)
+        world.set(GameConfigSingleton, GameConfigSingleton, game_config)
+        world.set(VectorMapSingleton, VectorMapSingleton, vector_map)
         
-        for _ in range(1, config.game.maximum_food):
-            spawn_food(world, config, vector_map)
+        for _ in range(game_config.maximum_food):
+            spawn_food(world, game_config, vector_map)
 
         pass
 
@@ -299,7 +303,7 @@ class GameInstance():
         world.set(entity, Name, name)
 
         position = Vector()
-        glob_entity = spawn_player(world, entity, self.config.game.starting_mass, position)
+        glob_entity = spawn_player(world, entity, self.game_config.starting_mass, position)
 
         print(f"* created entity: {glob_entity} ({name})")
 
@@ -309,7 +313,7 @@ class GameInstance():
             raise Exception
         
         world = self.world
-        target_position = point_to_vector(self.config, target_point)
+        target_position = point_to_vector(self.game_config, target_point)
 
         for glob_entity, position in Query(world, Position).with_ids(entity):
             direction = (target_position - position)
@@ -324,13 +328,13 @@ class GameInstance():
             raise Exception
         
         world = self.world
-        config = self.config
-        vector_map = self.vector_map
-        target_position = point_to_vector(self.config, target_point)
+        game_config = self.game_config
+        food_vector_map = self._food_vector_map
+        target_position = point_to_vector(game_config, target_point)
 
-        food_mass = config.game.food_mass
-        food_diameter = mass_to_radius(config, config.game.food_mass) * 2 
-        minimum_mass = config.game.minimum_mass
+        food_mass = game_config.food_mass
+        food_diameter = mass_to_radius(game_config, game_config.food_mass) * 2 
+        minimum_mass = game_config.minimum_mass
 
         for glob_entity, mass, position in Query(world, Mass, Position).with_ids(entity):
             if (mass <= minimum_mass):
@@ -342,11 +346,11 @@ class GameInstance():
             else:
                 direction = vector.normalize(direction)
 
-            radius = mass_to_radius(config, mass)
+            radius = mass_to_radius(game_config, mass)
             spawn_offset = (direction * (radius + food_diameter))
             spawn_position = position + spawn_offset
 
-            food_entity = spawn_food(world, config, vector_map)
+            food_entity = spawn_food(world, game_config, food_vector_map)
 
             world.set(glob_entity, Mass, max(minimum_mass, mass - food_mass))
             world.set(food_entity, Position, spawn_position)
@@ -358,11 +362,11 @@ class GameInstance():
             raise Exception
         
         world = self.world
-        config = self.config
-        target_position = point_to_vector(self.config, target_point)
+        game_config = self.game_config
+        target_position = point_to_vector(game_config, target_point)
 
-        minimum_mass = config.game.minimum_mass
-        merge_debounce = config.game.merge_debounce
+        minimum_mass = game_config.minimum_mass
+        merge_debounce = game_config.merge_debounce
 
         for original_entity, mass, position in Query(world, Mass, Position).with_ids(entity):
             half_mass = (mass / 2)
@@ -375,7 +379,7 @@ class GameInstance():
             else:
                 direction = vector.normalize(direction)
 
-            radius = mass_to_radius(config, half_mass)
+            radius = mass_to_radius(game_config, half_mass)
             position += direction * radius
 
             split_entity = spawn_player(world, entity, half_mass, position)
@@ -385,11 +389,10 @@ class GameInstance():
 
     async def init_game_loop(self):
         world = self.world
-        config = self.config
         socket = self.socket
+        tick_rate = self.tick_rate
 
         last_time = time()
-        tick_rate = (1.0 / config.server.update_rate)
         server_time = 0.0
 
         while True:
